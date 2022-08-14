602dfd016e340c9e097c506f9e73cca5ce89ec36
Mehul Fadnavis <fadnavismehul@gmail.com>
2022-08-14 23:29:20 +0800

    Latest Version

---
+++ data_cleaning.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+column_names = [
+    "age",
+    "workclass",
+    "fnlwgt",
+    "education",
+    "education-num",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "capital-gain",
+    "capital-loss",
+    "hours-per-week",
+    "native-country"
+]
+
+df = pd.read_csv('adult.data',header=None)
+
+df.columns = column_names
+
