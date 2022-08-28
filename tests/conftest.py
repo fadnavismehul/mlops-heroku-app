aedb216663fc2fba4b14881d0f81f2f76e0a27b2
Mehul Fadnavis <fadnavismehul@gmail.com>
2022-08-28 17:15:45 +0800

    Stopped tracking census_cleaned.csv from DVC

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+import pandas as pd
+
+@pytest.fixture(scope='session')
+def data():
+
+    # Download input artifact. This will also note that this script is using this
+    # particular version of the artifact
+    data_path = 'data/census_cleaned.csv'
+    df = pd.read_csv(data_path)
+
+    if data_path is None:
+        pytest.fail("You must provide the --csv option on the command line")
+
+    df = pd.read_csv(data_path)
+
+    return df
